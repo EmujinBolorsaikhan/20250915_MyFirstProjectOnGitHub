22553a457ca4302d3e83e75b850666820b245007
e.bolorsaikhan <emujinbolorsaikhan@gmail.com>
2025-09-16 12:52:57 +0200

    jetzt funktioniert es

--- Program.cs
+++ Program.cs
@@ -1,5 +1,1 @@
-﻿Console.WriteLine("Version #1");
-Console.WriteLine("add_something #1");
-Console.WriteLine("add_something #2");
-Console.WriteLine("new_branch1_#1");
-Console.WriteLine("new_branch1_#2");
+﻿Console.WriteLine("jetzt funktioniert es");
